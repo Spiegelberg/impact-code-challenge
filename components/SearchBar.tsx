4e5af0d71e8d7b6b57a15c4c071ce3e613eff843
Spiegelberg <jesperspiegelberg@gmail.com>
2023-11-27 10:20:14 +0100

    adding load indicator on searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -4,9 +4,10 @@ import useSearchQueryFromURL from '../hooks/useSearchQueryFromURL';
 
 interface SearchBarProps {
   onSearch: (query: string) => void;
+  loading: boolean;
 }
 
-const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({ onSearch }) => {
+const SearchBar: React.FC<SearchBarProps> = ({ onSearch, loading }) => {
   const [searchQuery, setSearchQuery] = useState('');
   const router = useRouter();
   
@@ -21,8 +22,10 @@ const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({ onSearch }
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
       if (router.pathname === '/') {
+        handleSearch();
         // If on the home page, navigate to /books with the search query
         router.push(`/books?q=${searchQuery}`);
+        
       } else {
         // If not on the home page, just trigger the search
         handleSearch();
@@ -54,10 +57,17 @@ const SearchBar: React.FC<{ onSearch: (query: string) => void }> = ({ onSearch }
         onClick={handleSearch}
         className="bg-blue-500 text-white px-4 py-2 "
       >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
-
+        {loading ? (
+          <svg className="animate-spin h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+          </svg>
+        )}
+        
       </button>
     </div>
   );
