3f2274cd5a9441a21e67c6ad3d341c767732c0c3
Spiegelberg <jesperspiegelberg@gmail.com>
2023-11-27 11:28:35 +0100

    adding drop-shadow global

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      dropShadow: {
+        'special': '10px 10px 5px rgba(0, 0, 0, 0.20)',
+      }
     },
   },
   plugins: [],
